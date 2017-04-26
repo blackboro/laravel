f7275efc21c5c9c7917cb86006167190f972d7a8
Goran Spudic <goran.spu@gmail.com>
2017-04-26 20:50:11 +0200

    Dodan view za show item

---
+++ resources/views/items/show.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>LARAVEL PROJEKT ITEM</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+     
+	  
+    </head>
+    <body>
+	
+	<h2> {{ $item->title }} </h2>
+	
+	<div>{{ $item->content }} </div>
+	
+	
+    </body>
+</html>
